8a453a927c69b359fb6e0adb7b4e3f9effaf695b
Bao Le <quocbao.lehoang@gmail.com>
2021-09-10 13:31:46 +0000

    toggleterm: I can float now haha :>

--- init.lua
+++ init.lua
@@ -54,7 +54,9 @@ require 'paq' {
   {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
-  }
+  };
+-- Terminal
+  'akinsho/toggleterm.nvim';
 }
 
 --------- hoob3rt/lualine.nvim ---------
@@ -214,6 +216,39 @@ require'nvim-treesitter.configs'.setup {
   }
 }
 --------- nvim-treesitter/nvim-treesitter ---------
+
+--------- akinsho/toggleterm.nvim ---------
+require'toggleterm'.setup {
+  -- size can be a number or function which is passed the current terminal
+  -- size = 20 | function(term)
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
+  open_mapping = [[<C-\>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  shade_terminals = true,
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  persist_size = true,
+  direction = 'float', -- 'vertical' | 'horizontal' | 'window' | 'float',
+  float_opts = {
+    -- The border key is *almost* the same as 'nvim_open_win'
+    -- see :h nvim_open_win for details on borders however
+    -- the 'curved' border is a custom border type
+    -- not natively supported but implemented in this plugin.
+    border = 'curved', -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+    -- width = <value>,
+    -- height = <value>,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    }
+  }
+}
+--------- akinsho/toggleterm.nvim ---------
 
 --------- Options ---------
 local indent = 2
