76c01458cf1d446024a0763d91356a9b82ec40cb
Bao Le <quocbao.lehoang@gmail.com>
2021-09-16 04:00:11 +0000

    Add `<C-q>` and `<C-S-q>`.

--- lua/general/mapping.lua
+++ lua/general/mapping.lua
@@ -22,11 +22,15 @@ map('n', '<S-Tab>', ':bp<CR>') -- Shift-Tab to previous buffer
 map('n', '<Tab>', ':bn<CR>') -- Tab to next buffer
 
 --[[
- ╔═══════╗
- ║ Save  ║
- ╚═══════╝
+ ╔════════════════╗
+ ║ Save and quit  ║
+ ╚════════════════╝
 --]]
 map('n', '<C-s>', ':w<CR>', {silent = false})
 map('i', '<C-s>', '<Esc>:w<CR>', {silent = false})
 map('n', '<C-S-s>', ':wa<CR>', {silent = false})
 map('i', '<C-S-s>', '<Esc>:wa<CR>', {silent = false})
+map('n', '<C-q>', ':q<CR>', {silent = false})
+map('i', '<C-q>', '<Esc>:q<CR>', {silent = false})
+map('n', '<C-S-q>', ':qa<CR>', {silent = false})
+map('i', '<C-S-q>', '<Esc>:qa<CR>', {silent = false})
