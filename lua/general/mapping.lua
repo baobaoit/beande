57ac16ba509524a9875ab6ca21ff933e432c0499
Bao Le <quocbao.lehoang@gmail.com>
2021-09-23 12:29:32 +0000

    Easy to navigate.

--- lua/general/mapping.lua
+++ lua/general/mapping.lua
@@ -42,3 +42,13 @@ map('n', '<C-q>', ':q<CR>', {silent = false})
 map('i', '<C-q>', '<Esc>:q<CR>', {silent = false})
 map('n', '<C-S-q>', ':qa<CR>', {silent = false})
 map('i', '<C-S-q>', '<Esc>:qa<CR>', {silent = false})
+
+--[[
+ ╔════════════════════════╗
+ ║ Switch Neovim's window ║
+ ╚════════════════════════╝
+--]]
+map('n', '<M-j>', '<C-w>j')
+map('n', '<M-k>', '<C-w>k')
+map('n', '<M-h>', '<C-w>h')
+map('n', '<M-l>', '<C-w>l')
