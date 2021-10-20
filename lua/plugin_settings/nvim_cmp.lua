fa53d7d6c7b34f034126239f1de0341502fb4755
Bao Le <quocbao.lehoang@gmail.com>
2021-10-20 15:41:42 +0700

    nvim-cmp: Fixed <Tab> and <S-Tab> not working after updated.

--- lua/plugin_settings/nvim_cmp.lua
+++ lua/plugin_settings/nvim_cmp.lua
@@ -3,9 +3,14 @@
  ║ Settings for hrsh7th/nvim-cmp ║
  ╚═══════════════════════════════╝
 --]]
-local fn = vim.fn
 local cmp = require "cmp"
 local luasnip = require "luasnip"
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -21,24 +26,27 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<Tab>"] = function(fallback)
-      if fn.pumvisible() == 1 then
-        fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if fn.pumvisible() == 1 then
-        fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        luasnip.jump(-1)
       else
         fallback()
       end
-    end,
+    end, { "i", "s" }),
   },
   formatting = {
     format = function(entry, vim_item)
@@ -51,7 +59,6 @@ cmp.setup {
         nvim_lsp = "[LSP]",
         luasnip = "[LuaSnip]",
         nvim_lua = "[Lua]",
-        latex_symbols = "[Latex]",
       })[entry.source.name]
       return vim_item
     end,
