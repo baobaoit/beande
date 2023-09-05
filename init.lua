4ca4ec365f1f074591f4a3321b9eee73134dfb8e
Bao Le <quocbao.lehoang@gmail.com>
2023-09-05 10:17:36 +0700

    chore(telescope.nvim): upgrade to v0.1.2.

--- init.lua
+++ init.lua
@@ -112,7 +112,7 @@ require('packer').startup(function(use)
 
   -- Telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
   -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
